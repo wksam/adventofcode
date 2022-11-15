13ef237b0260f29a7e3e9dfcae417223bc6b4e77
Sam <wk.sams@gmail.com>
2022-11-14 20:36:12 -0800

    Create Day 1 Sonar Sweep Part One in Javascript

---
+++ day_1_sonar_sweep/day_1_sonar_sweep_part_1.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+async function readFile(fileName, encoding='utf-8') {
+    const data = await fs.promises.readFile(fileName, encoding)
+    return data.split('\n').map((value) => parseInt(value))
+}
+
+async function sonarSweep(inputPath) {
+    const inputs = await readFile(inputPath)
+
+    const [,count] = inputs.slice(1).reduce((previousValue, currentValue) => 
+        [currentValue, currentValue > previousValue[0] ? previousValue[1] + 1 : previousValue[1]], [inputs[0], 0])
+
+    return count
+}
+
+const path = 'day_1_sonar_sweep/input.txt'
+sonarSweep(path).then((result) => console.log(result))
