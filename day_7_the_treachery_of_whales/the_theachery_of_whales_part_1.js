d7c7f026491fc0bf877b5b4070093c555acb9f4a
Sam <wk.sams@gmail.com>
2022-11-28 21:15:23 -0800

    Create Day 7 The Treachery of Whales Part 1 in Javascript

---
+++ day_7_the_treachery_of_whales/the_theachery_of_whales_part_1.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+async function readFile(fileName, encoding='utf-8') {
+    const data = await fs.promises.readFile(fileName, encoding)
+    return data.split(',').map(element => parseInt(element)).sort((a, b) => a - b)
+}
+
+async function leastFuelPossible(fileName) {
+    const crabsPosition = await readFile(fileName)
+    const median = crabsPosition[Math.floor((crabsPosition.length - 1) / 2)]
+    return crabsPosition.reduce((previousValue, currentValue) => previousValue + Math.abs(currentValue - median), 0)
+}
+
+const fileName = 'day_7_the_treachery_of_whales/crabs_position.txt'
+leastFuelPossible(fileName).then((result) => console.log(result))
