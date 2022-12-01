116d9d1b88ca350ad184f9aaf60462895c5762e6
Sam <wk.sams@gmail.com>
2022-12-01 08:54:53 -0800

    Move all folders to 2021 folder

---
+++ 2021/day_9_smoke_basin/smoke_basin_part_2.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+async function readFile(fileName, encoding='utf-8') {
+    const data = await fs.promises.readFile(fileName, encoding)
+    return data.split(/\r?\n/).map((line) => line.split('').map((digit) => parseInt(digit)))
+}
+
+async function calculateBasinsSize(fileName) {
+    const heightmap = await readFile(fileName)
+
+    let riskLevel = 0
+    for (let y = 0; y < heightmap.length; y++) {
+        for (let x = 0; x < heightmap[y].length; x++) {
+            if(isLowPoint(heightmap, x, y))
+                riskLevel += heightmap[y][x] + 1
+        }
+    }
+
+    return riskLevel
+}
+
+function isLowPoint(map, x, y) {
+    if(x - 1 >= 0 && map[y][x] >= map[y][x - 1])
+        return false
+    if(x + 1 < map[y].length && map[y][x] >= map[y][x + 1])
+        return false
+    if(y - 1 >= 0 && map[y][x] >= map[y - 1][x])
+        return false
+    if(y + 1 < map.length && map[y][x] >= map[y + 1][x])
+        return false
+    return true
+}
+
+function basinSize(map, x, y) {
+
+}
+
+const fileName = 'day_9_smoke_basin/heightmap.txt'
+calculateBasinsSize(fileName).then((result) => console.log(result))
