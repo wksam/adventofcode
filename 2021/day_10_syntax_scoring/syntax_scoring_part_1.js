4c9d619207ec282fb64728dfb2dc671c59e38f6a
Sam <wk.sams@gmail.com>
2022-12-02 13:58:02 -0800

    Create Day 10 Syntax Scoring Part 1 in Javascript

---
+++ 2021/day_10_syntax_scoring/syntax_scoring_part_1.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+
+async function readFile(fileName, encoding='utf-8') {
+    const data = await fs.promises.readFile(fileName, encoding)
+    return data.split(/\r?\n/)
+}
+
+async function calculateRiskLevel(fileName) {
+    const navigation_subsystem = await readFile(fileName)
+    const stack = []
+    let score = 0
+    for (const line of navigation_subsystem) {
+        for (const symbol of line) {
+            const numSymbol = convert(symbol)
+            if(numSymbol < 0)
+                stack.push(numSymbol)
+            else {
+                const lastOpenBracket = stack.pop()
+                if(lastOpenBracket + numSymbol == 0)
+                    continue
+                score += numSymbol
+                break
+            }
+        }
+    }
+    return score
+}
+
+function convert(symbol) {
+    switch (symbol) {
+        case '(': return -3
+        case ')': return 3
+        case '[': return -57
+        case ']': return 57
+        case '{': return -1197
+        case '}': return 1197
+        case '<': return -25137
+        case '>': return 25137
+    }
+}
+
+const fileName = '2021/day_10_syntax_scoring/navigation_subsystem.txt'
+calculateRiskLevel(fileName).then((result) => console.log(result))
