348a00d2f0b52241bcfdb7c10c348e4f2aaeab2f
Sam <wk.sams@gmail.com>
2022-12-02 12:19:51 -0800

    Refactoring enum

--- 2022/day_2_rock_paper_scissors/rock_paper_scissors_part_1.py
+++ 2022/day_2_rock_paper_scissors/rock_paper_scissors_part_1.py
@@ -12,9 +12,9 @@ def format_input(input):
     return [[convert(shape) for shape in round.split(' ')] for round in input.splitlines()]
 
 def convert(shape):
-    if shape == 'A' or shape == 'X': return 1
-    if shape == 'B' or shape == 'Y': return 2
-    if shape == 'C' or shape == 'Z': return 3
+    if shape == 'A' or shape == 'X': return Shape.ROCK
+    if shape == 'B' or shape == 'Y': return Shape.PAPER
+    if shape == 'C' or shape == 'Z': return Shape.SCISSORS
 
 def decrypt_strategy_guide(path):
     file = read_file(path)
@@ -26,13 +26,13 @@ def decrypt_strategy_guide(path):
             score += 6
         elif draw(player, opponent):
             score += 3
-        score += player
+        score += player.value
     return score
 
 def won(player, opponent):
-    return ((player == Shape.ROCK.value and opponent == Shape.SCISSORS.value) or 
-        (player == Shape.PAPER.value and opponent == Shape.ROCK.value) or
-        (player == Shape.SCISSORS.value and opponent == Shape.PAPER.value))
+    return ((player == Shape.ROCK and opponent == Shape.SCISSORS) or 
+        (player == Shape.PAPER and opponent == Shape.ROCK) or
+        (player == Shape.SCISSORS and opponent == Shape.PAPER))
 
 def draw(player, opponent):
     return player == opponent
